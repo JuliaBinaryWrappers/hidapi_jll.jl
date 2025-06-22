c02273453ff0c8990ef6c2e5d0c243a981256fe4
jlbuild <juliabuildbot@gmail.com>
2025-06-22 09:58:33 +0000

    hidapi_jll build 0.15.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         hidapi,
-        "lib/libhidapi.0.14.0.dylib",
+        "lib/libhidapi.0.15.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
